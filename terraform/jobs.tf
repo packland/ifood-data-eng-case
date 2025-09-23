c8b22a7d1978c620c4f939fd63b313ca8f220005
Patrick Silva <packland@gmail.com>
2025-09-22 22:33:57 -0300

    job id

--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -9,12 +9,11 @@ resource "databricks_repo" "repo_ifood_case" {
 
 resource "databricks_job" "job_ingestao_bronze" {
   name                = "1 - [iFood Case] - Ingestão Camada Bronze"
-  existing_cluster_id = databricks_cluster.cluster_analise.id
 
   task {
     task_key = "ingestao_bronze"
+    existing_cluster_id = databricks_cluster.cluster_analise.id
     notebook_task {
-      # Isso continuará funcionando perfeitamente.
       notebook_path = "${databricks_repo.repo_ifood_case.path}/main_flow.ipynb"
     }
   }
